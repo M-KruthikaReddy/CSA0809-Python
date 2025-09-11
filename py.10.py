6b870f84b58c2447f1a73734a60d4fb0bf97bde9
M-KruthikaReddy <kruthivenumanda@gmail.com>
2025-09-11 13:45:00 +0530

    Add files via upload

---
+++ py.10.py
@@ -0,0 +1,5 @@
+l=int(input("Enter length:"))
+b=int(input("Enter breadth:"))
+h=int(input("Enter height"))
+a=l*b*h
+print("Volume:",a)
---
+++ py.8.py
@@ -0,0 +1,4 @@
+r=int(input("enter radius:"))
+l=int(input("enter length:"))
+a=3.14*r*(r+l)
+print("Area of TSA cone:",a)
